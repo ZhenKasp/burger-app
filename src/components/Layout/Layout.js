58efbeb00a17fa3f01a4fd6d25c230fac3b87417
Evgeny A. Kasperovich <zhenkasp@mail.ru>
2020-09-22 14:44:14 +0300

    show burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Auxiliary from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.module.css'
 
 const layout = (props) => (
-    <Auxiliary>
+    <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
-    </Auxiliary>
+    </Aux>
     
 );
 
